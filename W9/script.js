98f7b4b00301aa9e41d31f22c456fcf5bf42df0d
PhuongAnh2212 <phamphuonganh251@gmail.com>
2024-12-13 08:21:54 +0700

    em quên up lên github

--- W9/script.js
+++ W9/script.js
@@ -1,6 +1,7 @@
 function calculate(operator) {
     const a = parseFloat(document.getElementById('a').value);
     const b = parseFloat(document.getElementById('b').value);
+    const c = parseFloat(document.getElementById('c').value);
     const resultDiv = document.getElementById('result');
 
     // Validation
@@ -14,14 +15,13 @@ function calculate(operator) {
       return;
     }
 
-    // Perform calculation
     let result;
     switch (operator) {
-      case '+': result = a + b; 
+      case '+': result = a + b+ c; 
        break;
-      case '-': result = a - b;
+      case '-': result = a - b- c;
        break;
-      case '*': result = a * b; 
+      case '*': result = a * b * c; 
        break;
       case '/': result = a / b;
        break;
